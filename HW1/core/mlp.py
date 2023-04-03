b0a1acc38d0cd8586fa67ffe921156002d3f6c9b
Arya-Ebrahimi <aryaebrahimi3000@mail.um.ac.ir>
2023-04-04 00:21:41 +0430

    add more configs

--- HW1/core/mlp.py
+++ HW1/core/mlp.py
@@ -27,7 +27,7 @@ class NeuralNet(tf.keras.Model):
         return x
 
 class MLP():
-    def __init__(self, config):
+    def __init__(self, config, name):
         self.learning_rate = config['learning_rate']
         self.num_classes = config['num_classes']
         self.num_features = config['num_features']
@@ -46,15 +46,12 @@ class MLP():
         elif optim == 'rmsprop':
             self.optimizer = tf.keras.optimizers.RMSprop(self.learning_rate)
             
-        self.train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
-        self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('train_accuracy')
-        self.test_loss = tf.keras.metrics.Mean('test_loss', dtype=tf.float32)
-        self.test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('test_accuracy')
-        self.name = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-        self.train_log_dir = 'logs/' + self.name + '/train'
-        self.test_log_dir = 'logs/' + self.name + '/test'
+        self.name = name + '-' + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        self.train_log_dir = 'logs/train/' + self.name
+        self.test_log_dir = 'logs/test/' + self.name 
         self.train_summary_writer = tf.summary.create_file_writer(self.train_log_dir)
-        
+        self.test_summary_writer = tf.summary.create_file_writer(self.test_log_dir)
+
     def _load_data(self):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path='/home/arya/Desktop/DL/HW1/dataset/mnist.npz')
         x_train, x_test = np.array(x_train, np.float32), np.array(x_test, np.float32)
@@ -85,25 +82,29 @@ class MLP():
             gradients = tape.gradient(loss, trainable_vars) 
             self.optimizer.apply_gradients(zip(gradients, trainable_vars))
             
-            self.train_loss(loss)
-            self.train_accuracy(pred, y)
             
     def train(self):
         for step, (batch_x, batch_y) in enumerate(self.train_data.take(self.training_steps), 1):
             self._optimize(batch_x, batch_y)
             
             if step % self.save_ratio == 0:    
-                with self.train_summary_writer.as_default():
-                    tf.summary.scalar('loss', self.train_loss.result(), step=step)
-                    tf.summary.scalar('accuracy', self.train_accuracy.result(), step=step)
-                    
-                self.train_loss.reset_states()
-                self.train_accuracy.reset_states()
-                
-                pred = self.model(batch_x, is_training=True)
+
+                pred = self.model(batch_x)
                 loss = self._loss_fn(pred, batch_y)
                 acc = self._accuracy(pred, batch_y)
                 print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
+                
+                with self.train_summary_writer.as_default():
+                    tf.summary.scalar('loss', loss, step=step)
+                    tf.summary.scalar('accuracy', acc, step=step)
+                    
+                test_pred = self.model(self.x_test)
+                test_loss = self._loss_fn(test_pred, self.y_test)
+                test_acc = self._accuracy(test_pred, self.y_test)
+                
+                with self.test_summary_writer.as_default():
+                    tf.summary.scalar('loss', test_loss, step=step)
+                    tf.summary.scalar('accuracy', test_acc, step=step)
                 
         print('training completed')
         
--- HW1/main.py
+++ HW1/main.py
@@ -1,10 +1,14 @@
 from core.mlp import MLP
 from core.config import parse_config
+import sys
 
-config_name = 'config1'
+if sys.argv[1] != None:
+    config_name = sys.argv[1]
+else:
+    config_name = 'config1'
 
 
 config = parse_config(config_name=config_name)
 
-mlp = MLP(config=config)
+mlp = MLP(config=config, name=config_name)
 mlp.train()
